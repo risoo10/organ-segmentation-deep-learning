aab4860ad2555136d51068f50a1510a35040a8fe
Richard Mocak <richard.mocak@gmail.com>
2020-03-18 12:16:05 +0100

    Formatting file

--- lits/preprocess.py
+++ lits/preprocess.py
@@ -3,35 +3,40 @@ import os
 import nibabel as nib
 import numpy as np
 
+
 def load_as_npy(path):
     return nib.load(path).get_fdata()
 
+
 def load_lits_files(path):
-  sets = os.listdir(path)
-  files = []
-  for folder in sets:
-    folder_files = os.listdir(os.path.join(path, folder))
-    files.extend(list(map(lambda x: os.path.join(folder, x), folder_files)))
+    sets = os.listdir(path)
+    files = []
+    for folder in sets:
+        folder_files = os.listdir(os.path.join(path, folder))
+        files.extend(
+            list(map(lambda x: os.path.join(folder, x), folder_files)))
 
-  volumes = list(filter(lambda x: 'volume' in x, files))
-  volumes.sort()
+    volumes = list(filter(lambda x: 'volume' in x, files))
+    volumes.sort()
 
-  segmentations = list(filter(lambda x: 'segmentation' in x, files))
-  segmentations.sort()
+    segmentations = list(filter(lambda x: 'segmentation' in x, files))
+    segmentations.sort()
 
-  print('volumes:', len(volumes), '| exmp.',  volumes[:5])
-  print('segmentations:', len(segmentations), '| exmp.',  segmentations[:5])
+    print('volumes:', len(volumes), '| exmp.',  volumes[:5])
+    print('segmentations:', len(segmentations), '| exmp.',  segmentations[:5])
 
-  return volumes, segmentations
+    return volumes, segmentations
+
 
 def plot_slice(volume, segm, slice_index):
-  print(volume.shape)
-  print('seg [min, max]', segm[:, :, slice_index].min(), segm[:, :, slice_index].max())
-  plt.figure(figsize=(15, 15))
-  plt.subplot('121')
-  plt.title('Source')
-  plt.imshow(volume[:, :, slice_index], cmap="bone",)
-  plt.subplot('122')
-  plt.title('Segmentation')
-  seg = segm[:, :, slice_index]
-  plt.imshow(seg == 1, cmap="tab20")
+    print(volume.shape)
+    print('seg [min, max]', segm[:, :, slice_index].min(),
+          segm[:, :, slice_index].max())
+    plt.figure(figsize=(15, 15))
+    plt.subplot('121')
+    plt.title('Source')
+    plt.imshow(volume[:, :, slice_index], cmap="bone",)
+    plt.subplot('122')
+    plt.title('Segmentation')
+    seg = segm[:, :, slice_index]
+    plt.imshow(seg == 1, cmap="tab20")
