268cb982767fbe68e127a4be7635af8a8b0ab76f
Richard Mocak <richard.mocak@gmail.com>
2020-04-18 22:25:01 +0200

    Append transforms to constants.

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,15 @@
+import torch
+from torchvision.transforms import Compose
+from inception.transforms import ReshapeTransform, CustomResize, CatTransform
+
 PATIENTS, SAMPLES, WIDTH, HEIGHT = 131, 58638, 512, 512
 
 drive_dir = '/content/drive/My Drive/DP'
+
+resize_transform = Compose([
+        torch.from_numpy,
+        ReshapeTransform((1, 1, WIDTH, HEIGHT)),
+        CustomResize(size=(299, 299)),
+        ReshapeTransform((1, 299, 299)),
+        CatTransform(3),
+])
