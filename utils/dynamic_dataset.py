dc117906785e3680492602986f9698be6717088e
Richard Mocak <richard.mocak@gmail.com>
2020-05-02 18:41:41 +0200

    Added Dynamic Set for fast loading.

--- utils/dynamic_dataset.py
+++ utils/dynamic_dataset.py
@@ -2,77 +2,116 @@ import h5py
 from utils.constants import *
 import numpy as np
 import pickle
+from torch.utils.data import Dataset
+from utils.utils import distance_transform_weight
 
 
-class DynamicDataset():
-    def __init__(self, filepath, shape, sequences, dir=drive_dir):
-        self.opened = False
-        self.filename = filepath
-        self.file = None
-        self.x = None
-        self.y = None
-        self.slices = None
-        self.cropped_slices = None
-        self.train = None
-        self.val = None
-        self.test = None
-        self.shape = shape
-        self.sequences = sequences
-        self.dir = dir
+class Slice:
+    def __init__(self, id, array_slice):
+        self.id = id
+        self.array_slice = array_slice
 
-    def load_write(self):
-        self.file = h5py.File(f'{self.dir}/{self.filename}.hdf5', mode="w")
-        self.opened = True
 
-        self.x = self.file.create_dataset(
-            'x', shape=self.shape, dtype=np.float16, compression='gzip')
-        self.y = self.file.create_dataset(
-            'y', shape=self.shape, dtype=np.int8, compression='gzip')
-        self.slices = self.file.create_dataset(
-            'slices', shape=(self.sequences, 2), dtype=np.int8, compression='gzip')
+class DynamicSet(Dataset):
+    def __init__(
+        self,
+        set,
+        dset_file,
+        cropped=False,
+        weights=False,
+        transform=torch.from_numpy,
+        classification=False,
+        augmentation=None,
+        plane="horizontal"
+    ):
 
-    def write_splits(self):
-        self.train, self.test, self.val = self.split_test_val_train(
-            self.sequences)
-        self.write_pickle_data()
+        assert set in ['train', 'test', 'val']
+        self.set = set
+        ind = dset_file[set][:]
+        self.dset_file = dset_file
+        self.cropped = cropped
+        self.transform = transform
+        self.weights = weights
+        self.classification = classification
+        self.augmentation = augmentation
+        self.empty_weight = torch.Tensor()
+        self.plane = plane
 
-    def write_cropped_slices(self, slcs):
-        self.cropped_slices = slcs
-        self.write_pickle_data()
+        if cropped:
+            self.ind_map = self.crop_item_slice(ind)
+        else:
+            self.ind_map = self.map_item_slice(ind)
 
-    def write_pickle_data(self):
-        data = {}
-        data["train"] = self.train
-        data["test"] = self.test
-        data["val"] = self.val
-        data["cropped_slices"] = self.cropped_slices
-        pickle.dump(data, open(f'{self.dir}/{self.filename}.p', "wb"))
+        self.length = len(self.ind_map)
 
-    def load(self, mode):
-        self.file = h5py.File(
-            f'{self.dir}/{self.filename}.hdf5', mode=mode)
-        self.opened = True
-        self.x = self.file['x']
-        self.y = self.file['y']
-        self.slices = self.file['slices']
+    def __len__(self):
+        return len(self.ind_map)
 
-        try:
-            data = pickle.load(open(f'{self.dir}/{self.filename}.p', "rb"))
-            self.train = data['train']
-            self.test = data['test']
-            self.val = data['val']
-            self.cropped_slices = data['cropped_slices']
-        except:
-            print('Tran or Test or Val or Cropped slices not set')
+    def __getitem__(self, i):
+        ind = self.ind_map[i]
+        x = self.dset_file[ind.id]['x'][ind.array_slice]
+        x = x[np.newaxis, :].astype(np.float32)
+        _y = self.dset_file[ind.id]['y'][ind.array_slice]
 
-    def save(self, x, y, start, end, patient_ind):
-        self.x[start:end] = x
-        self.y[start:end] = y
-        self.slices[patient_ind] = np.array([start, end]).astype(np.int32)
+        if self.augmentation != None:
+            aug = self.augmentation(image=x, mask=_y)
+            x = aug["image"]
+            _y = aug["mask"]
 
-    def split_test_val_train(self, length):
-        indices = np.arange(0, length)
-        np.random.seed(42)
-        np.random.shuffle(indices)
-        splits = [int(length * 0.7), int(length * 0.9)]
-        return np.split(indices, splits)
+        if self.classification:
+            y = np.array(np.any(_y)).astype(np.float32)
+            return self.transform(x), torch.from_numpy(y), self.empty_weight
+        else:
+            y = _y[np.newaxis, :].astype(np.float32)
+
+            if self.weights:
+                weight = distance_transform_weight(_y).astype(np.float32)
+                weight = np.nan_to_num(weight)
+                weight = weight[np.newaxis, :].astype(np.float32)
+                return self.transform(x), self.transform(y), self.transform(weight)
+            else:
+                return self.transform(x), self.transform(y), self.empty_weight
+
+    def crop_item_slice(self, ind):
+        ind_map = []
+        for i in ind:
+            id = str(i)
+            boundaries = self.dset_file[id]['cropped'][:].astype(np.int)
+            self.plane_slice(id, boundaries, ind_map)
+        return ind_map
+
+    def map_item_slice(self, ind):
+        ind_map = []
+        for i in ind:
+            id = str(i)
+            total = self.dset_file[id]['x'].shape[0]
+            boundaries = [0, total]
+            self.plane_slice(id, boundaries, ind_map)
+        return ind_map
+
+    def plane_slice(self, id, boundaries, ind_map):
+        if self.plane == 'horizontal':
+            self.horizontal_item_slice(ind_map, id, boundaries)
+        elif self.plane == 'frontal':
+            self.vertical_item_slice(ind_map, id, boundaries)
+        elif self.plane == 'sagital':
+            self.sagital_item_slice(ind_map, id, boundaries)
+
+    def horizontal_item_slice(self, ind_map, id, boundaries):
+        all = slice(0, WIDTH)
+        for x in range(boundaries[0], boundaries[1]):
+            ind_map.append(
+                Slice(id, (x, all, all))
+            )
+
+    def vertical_item_slice(self, ind_map,  id, boundaries):
+        all = slice(0, WIDTH)
+        z_slice = slice(boundaries[0], boundaries[1])
+        for i in range(WIDTH):
+            ind_map.append(Slice(id, (z_slice, i, all)))
+
+    def sagital_item_slice(self, ind_map,  id, boundaries):
+        z_slice = slice(boundaries[0], boundaries[1])
+        all = slice(0, HEIGHT)
+        for i in range(HEIGHT):
+            ind_map.append(Slice(id, (z_slice, all, i)))
